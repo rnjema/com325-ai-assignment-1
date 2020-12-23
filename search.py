8ebd3d6a595b1ccb2f7ed3af38a32cdfa9b40131
Ramsey Ith Njema Jr <ramseyith99@gmail.com>
2020-12-23 12:29:39 +0200

    Implemented Breadth First Search

--- search.py
+++ search.py
@@ -126,9 +126,11 @@ def depthFirstSearch(problem):
     return generalSearch(problem, structure=stack) 
 
 def breadthFirstSearch(problem):
-    """Search the shallowest nodes in the search tree first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    """Searches the shallowest nodes in the search tree first. Employs a FIFO-queue""" 
+
+    queue = util.Queue() # Initializes an empty queue
+
+    return generalSearch(problem, structure=queue)
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
